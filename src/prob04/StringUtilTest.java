cdda0e058ea1bcbe11de401e08d0511582a44967
BIT <BIT@DESKTOP-08BCIT0>
2019-08-08 16:48:53 +0900

    20190808 practice03

--- src/prob04/StringUtilTest.java
+++ src/prob04/StringUtilTest.java
@@ -1,7 +1,7 @@
 package prob04;
 
 class StringUtil {
-	public static String a = null;
+	public static String a = "";
 	
     public static String concatenate(String[] str) {
         //문자열을 결합하여 리턴 하는 메소드 구현
