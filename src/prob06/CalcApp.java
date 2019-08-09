27cbeb279933fe4c9a551196841e1137605c3cba
BIT <BIT@DESKTOP-08BCIT0>
2019-08-09 17:08:00 +0900

    2019-08-09

--- src/prob06/CalcApp.java
+++ src/prob06/CalcApp.java
@@ -2,6 +2,126 @@ package prob06;
 
 import java.util.Scanner;
 
+class Add{
+    int a;
+    int b;
+   
+	public int getA() {
+		return a;
+	}
+	
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	public int getB() {
+		return b;
+	}
+	
+	public void setB(int b) {
+		this.b = b;
+	}
+	   
+	void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	int calculate() {
+		return a+b;	   
+	}
+	   
+
+}
+
+class Sub{
+    int a;
+    int b;
+   
+	public int getA() {
+		return a;
+	}
+	
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	public int getB() {
+		return b;
+	}
+	
+	public void setB(int b) {
+		this.b = b;
+	}
+	   
+	void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	int calculate() {
+		return a-b;	   
+	}
+}
+class Mul{
+    int a;
+    int b;
+   
+	public int getA() {
+		return a;
+	}
+	
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	public int getB() {
+		return b;
+	}
+	
+	public void setB(int b) {
+		this.b = b;
+	}
+	   
+	void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	int calculate() {
+		return a*b;	   
+	}
+}
+class Div {
+    int a;
+    int b;
+   
+	public int getA() {
+		return a;
+	}
+	
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	public int getB() {
+		return b;
+	}
+	
+	public void setB(int b) {
+		this.b = b;
+	}
+	   
+	void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	int calculate() {
+		return a/b;	   
+	}
+}
+
 public class CalcApp {
 
 	public static void main(String[] args) {
@@ -11,7 +131,7 @@ public class CalcApp {
 
 			System.out.print( ">> " );
 			String expression = scanner.nextLine();
-			
+				
 			if( "quit".equals( expression ) ) {
 				break;
 			}
@@ -23,7 +143,41 @@ public class CalcApp {
 				continue;
 			}
 			
-
+			int lvalue = Integer.parseInt(tokens[0]);
+			int rvalue = Integer.parseInt(tokens[2]);
+			
+			switch (tokens[1]) {
+				case "+": {
+					Add add = new Add();
+					add.setValue(lvalue,rvalue);
+					int result = add.calculate();
+					System.out.println(">>" + result);
+					break;
+				}
+				case "-": {
+					Sub sub = new Sub();
+					sub.setValue(lvalue,rvalue);
+					int result = sub.calculate();
+					System.out.println(">>" + result);
+					break;
+				}
+				case "*": {
+					Mul mul = new Mul();
+					mul.setValue(lvalue,rvalue);
+					int result = mul.calculate();
+					System.out.println(">>" + result);
+					break;
+				}
+				case "/": {
+					Div  div = new Div ();
+					div.setValue(lvalue,rvalue);
+					int result = div.calculate();
+					System.out.println(">>" + result);
+					break;
+				}
+				default:
+					break;
+			}
 			
 			/*  코드를 완성 합니다 */
 			
